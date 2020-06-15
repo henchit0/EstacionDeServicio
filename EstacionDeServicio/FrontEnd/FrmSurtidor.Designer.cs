78f3718cd8dadd30e7124925ad6f2a271bb61adc
dmarmori <dionel.marmori@iiconcepcion.edu.ar>
2020-06-15 13:18:14 -0300

    FinT

--- EstacionDeServicio/FrontEnd/FrmSurtidor.Designer.cs
+++ EstacionDeServicio/FrontEnd/FrmSurtidor.Designer.cs
@@ -42,14 +42,15 @@
 			this.lblDisponible = new System.Windows.Forms.Label();
 			this.lblCobrado = new System.Windows.Forms.Label();
 			this.gbxSurtidor = new System.Windows.Forms.GroupBox();
+			this.gbxTanque = new System.Windows.Forms.GroupBox();
 			this.gbxSurtidos = new System.Windows.Forms.GroupBox();
 			this.label1 = new System.Windows.Forms.Label();
-			this.gbxTanque = new System.Windows.Forms.GroupBox();
 			this.btnSurtir = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.numLitros)).BeginInit();
 			this.gbxSurtidor.SuspendLayout();
-			this.gbxSurtidos.SuspendLayout();
 			this.gbxTanque.SuspendLayout();
+			this.gbxSurtidos.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// lblSurtidor
@@ -205,6 +206,21 @@
 			this.gbxSurtidor.TabStop = false;
 			this.gbxSurtidor.Text = "Surtidor";
 			// 
+			// gbxTanque
+			// 
+			this.gbxTanque.BackColor = System.Drawing.SystemColors.GrayText;
+			this.gbxTanque.Controls.Add(this.lblDisponible);
+			this.gbxTanque.Controls.Add(this.lblPrecio);
+			this.gbxTanque.Controls.Add(this.lblCapacidad);
+			this.gbxTanque.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.gbxTanque.ForeColor = System.Drawing.Color.Navy;
+			this.gbxTanque.Location = new System.Drawing.Point(17, 56);
+			this.gbxTanque.Name = "gbxTanque";
+			this.gbxTanque.Size = new System.Drawing.Size(286, 142);
+			this.gbxTanque.TabIndex = 11;
+			this.gbxTanque.TabStop = false;
+			this.gbxTanque.Text = "Datos del tanque";
+			// 
 			// gbxSurtidos
 			// 
 			this.gbxSurtidos.BackColor = System.Drawing.SystemColors.GrayText;
@@ -232,21 +248,6 @@
 			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			this.label1.Click += new System.EventHandler(this.label1_Click_1);
 			// 
-			// gbxTanque
-			// 
-			this.gbxTanque.BackColor = System.Drawing.SystemColors.GrayText;
-			this.gbxTanque.Controls.Add(this.lblDisponible);
-			this.gbxTanque.Controls.Add(this.lblPrecio);
-			this.gbxTanque.Controls.Add(this.lblCapacidad);
-			this.gbxTanque.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.gbxTanque.ForeColor = System.Drawing.Color.Navy;
-			this.gbxTanque.Location = new System.Drawing.Point(17, 56);
-			this.gbxTanque.Name = "gbxTanque";
-			this.gbxTanque.Size = new System.Drawing.Size(286, 142);
-			this.gbxTanque.TabIndex = 11;
-			this.gbxTanque.TabStop = false;
-			this.gbxTanque.Text = "Datos del tanque";
-			// 
 			// btnSurtir
 			// 
 			this.btnSurtir.BackColor = System.Drawing.Color.Green;
@@ -261,6 +262,19 @@
 			this.btnSurtir.UseVisualStyleBackColor = false;
 			this.btnSurtir.Click += new System.EventHandler(this.btnSurtir_Click);
 			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.SlateBlue;
+			this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(489, 299);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(65, 28);
+			this.button1.TabIndex = 12;
+			this.button1.Text = "Volver";
+			this.button1.UseVisualStyleBackColor = false;
+			// 
 			// FrmSurtidor
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -268,6 +282,7 @@
 			this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
 			this.BackColor = System.Drawing.SystemColors.GrayText;
 			this.ClientSize = new System.Drawing.Size(566, 392);
+			this.Controls.Add(this.button1);
 			this.Controls.Add(this.gbxSurtidor);
 			this.Controls.Add(this.lblCobrado);
 			this.Controls.Add(this.btnVolver);
@@ -276,7 +291,7 @@
 			this.Controls.Add(this.btnSurtir);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Margin = new System.Windows.Forms.Padding(2);
 			this.Name = "FrmSurtidor";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "FrmSurtidor";
@@ -284,10 +299,10 @@
 			((System.ComponentModel.ISupportInitialize)(this.numLitros)).EndInit();
 			this.gbxSurtidor.ResumeLayout(false);
 			this.gbxSurtidor.PerformLayout();
-			this.gbxSurtidos.ResumeLayout(false);
-			this.gbxSurtidos.PerformLayout();
 			this.gbxTanque.ResumeLayout(false);
 			this.gbxTanque.PerformLayout();
+			this.gbxSurtidos.ResumeLayout(false);
+			this.gbxSurtidos.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -311,5 +326,6 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.GroupBox gbxTanque;
 		private System.Windows.Forms.Button btnSurtir;
+		private System.Windows.Forms.Button button1;
 	}
 }
